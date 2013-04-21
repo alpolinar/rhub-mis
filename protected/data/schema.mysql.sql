8914a02f44dcd1dcd4338e6b6de6679f226bee56
Joey O. Chua <joeycbulk@yahoo.com>
2013-04-21 13:45:42 +0800

    fixed bug mis schema

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS mis_user;
 CREATE TABLE mis_user (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(128) NOT NULL,
@@ -10,6 +11,7 @@ INSERT INTO mis_user (username, password, email) VALUES ('test1', 'pass1', 'test
 INSERT INTO mis_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
 INSERT INTO mis_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
 
+DROP TABLE IF EXISTS mis_tag_types;
 CREATE TABLE  mis_tag_types (
     id SERIAL,
     tag_type VARCHAR(100) NOT NULL,
@@ -17,14 +19,16 @@ CREATE TABLE  mis_tag_types (
     UNIQUE INDEX i_tag_type(tag_type) COMMENT 'prevent duplicate tag type'
 ) COMMENT 'example tag type are behavioral segmentation, demographic segmentation, etc.';
 
+DROP TABLE IF EXISTS mis_tags;
 CREATE TABLE mis_tags (
     id SERIAL,
     tag_type_id BIGINT UNSIGNED NOT NULL COMMENT 'a constant in the application',
     tag VARCHAR(100) NOT NULL,
     PRIMARY KEY(id),
-    UNIQUE INDEX i_unique_tag (tag_type, tag) COMMENT 'prevent duplicate tag under same type/segment'
+    UNIQUE INDEX i_unique_tag (tag_type_id, tag) COMMENT 'prevent duplicate tag under same type/segment'
 );
 
+DROP TABLE IF EXISTS mis_files;
 CREATE TABLE mis_files (
     id SERIAL,
     file_type_id BIGINT UNSIGNED NOT NULL COMMENT 'a constant in the application',
@@ -32,27 +36,28 @@ CREATE TABLE mis_files (
     description TEXT,
     start DATE COMMENT 'start date at which this file is effective',
     end DATE COMMENT 'end date at which this file is effective',
-    created DATETIME 'date the file was created',
-    updated TIMESTAMP 'date the file was last updated',
+    created DATETIME COMMENT 'date the file was created',
+    updated TIMESTAMP COMMENT 'date the file was last updated',
     PRIMARY KEY(id),
-    UNIQUE INDEX i_type(file_type_id, i_name) 'unique name per type',
+    UNIQUE INDEX i_type(file_type_id, name) COMMENT 'unique name per type',
     INDEX i_name(name),
-    FULLTEXT INDEX i_description(description),
     INDEX(start),
     INDEX(end)
 ) COMMENT 'common field among other tables';
 
+DROP TABLE IF EXISTS mis_file_tags;
 CREATE TABLE mis_file_tags (
     file_id BIGINT UNSIGNED NOT NULL,
     tag_id BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (file_id, tag_id)
 ) COMMENT 'list of tags assigned to file';
 
+DROP TABLE IF EXISTS mis_people;
 CREATE TABLE mis_people (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     population DOUBLE UNSIGNED NOT NULL,
-    age_start TINY INT UNSIGNED NOT NULL,
-    age_end TINY INT UNSIGNED NOT NULL,
+    age_start TINYINT UNSIGNED NOT NULL,
+    age_end TINYINT UNSIGNED NOT NULL,
     country CHAR(2),
     state CHAR(5),
     city VARCHAR(60),
@@ -67,24 +72,28 @@ CREATE TABLE mis_people (
     INDEX i_zip_code(zip_code)
 );
 
+DROP TABLE IF EXISTS mis_business_components;
 CREATE TABLE mis_business_components (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     value_propositions TEXT,
     PRIMARY KEY (file_id)
 );
 
+DROP TABLE IF EXISTS mis_external_environments;
 CREATE TABLE mis_external_environments (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     external_environment TEXT,
     PRIMARY KEY (file_id)
 );
 
+DROP TABLE IF EXISTS mis_target_markets;
 CREATE TABLE mis_target_markets (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     target_market TEXT,
     PRIMARY KEY (file_id)
 );
 
+DROP TABLE IF EXISTS mis_swot_tags;
 CREATE TABLE mis_swot_tags (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     tag_id BIGINT UNSIGNED NOT NULL,
@@ -93,10 +102,11 @@ CREATE TABLE mis_swot_tags (
     PRIMARY KEY (file_id, tag_id, area_id)
 );
 
+DROP TABLE IF EXISTS mis_swot_files;
 CREATE TABLE mis_swot_files (
     file_id BIGINT UNSIGNED NOT NULL COMMENT 'foreign key from files table',
     child_file_id BIGINT UNSIGNED NOT NULL COMMENT '',
     area_id INT UNSIGNED NOT NULL COMMENT 'indicator if tag is used for stregnths, weaknesses, opportunity, threats',
     description TEXT,
-    PRIMARY KEY (file_id, chile_file_id, area_id)
+    PRIMARY KEY (file_id, child_file_id, area_id)
 );
